b80a1c9db161e9824d709d4c4c19518b7ce71416
jaehyeok <jaehyeok7633@naver.com>
2024-05-03 15:08:22 +0900

    동적 클래스 변경, styled components 사용

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './CourseList.css';
+// import './CourseList.css';
+import CourseItem from './CourseItem';
+import styled from 'styled-components';
 
-const CourseList = () => {
+const CourseUl = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const CourseList = ({ items, onDelete }) => {
   return (
-    <ul className="goal-list">
-      <li></li>
-    </ul>
+    <CourseUl>
+      {items.map((item) => {
+        return <CourseItem key={item.id} item={item} onDelete={onDelete} />;
+      })}
+    </CourseUl>
   );
 };
 
